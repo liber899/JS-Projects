1f151f8cf60af9ec6487a2f77955b1e490fa7fb1
liber899 <80169201+liber899@users.noreply.github.com>
2022-07-21 15:39:18 +0700

    Update script.js

--- starter/script.js
+++ starter/script.js
@@ -34,26 +34,6 @@ document.querySelector('.check').addEventListener('click', function(){
      document.querySelector('.score').textContent = 0;
  }
     }
-    // else if (guess > secretNumber){
-    //     if (score > 1){
-    //     document.querySelector('.message'). textContent = 'Too high!';
-    //     score--; 
-    //     document.querySelector('.score').textContent = score;
-    //     } else {
-    //         document.querySelector('.message'). textContent = 'You lost the game!';
-    //         document.querySelector('.score').textContent = 0;
-    //     }
-
-    // } else if (guess < secretNumber){
-    //     if (score > 1){
-    //     document.querySelector('.message'). textContent = 'Too low!';
-    //     score--; 
-    //     document.querySelector('.score').textContent = score;
-    //     } else {
-    //         document.querySelector('.message'). textContent = 'You lost the game!';
-    //         document.querySelector('.score').textContent = 0;
-    //     }
-    // }
 })
 document.querySelector('.again').addEventListener('click',function(){
     score = 20;
